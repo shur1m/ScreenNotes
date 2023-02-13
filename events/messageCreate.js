01bb90f8dd6d178762c2da4158788cda67fe8d11
Anson Xu <85322327+shur1m@users.noreply.github.com>
2023-02-12 17:08:44 -0800

    reorganized code

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,34 @@
 const { Events, Attachment, AttachmentBuilder} = require('discord.js');
 const { convertImageToText } = require('../utils/tesseract/imageToText.js');
 
+async function transcribeImage(message){
+
+    //ensure safe word is not in message
+    if (!message.content.includes('donot')){
+        // for every attachment (picture), grab the links of the images
+        let imageLinks = [];
+        for (entry of message.attachments.entries()){
+            for (element of entry){
+                if (element instanceof Attachment) 
+                    imageLinks.push(element.attachment);
+            }
+        }
+
+        if (imageLinks.length > 0){
+            // convert each image to text
+            for (link of imageLinks){
+                let imageText = await convertImageToText(link);
+                const image = new AttachmentBuilder(link);
+                await message.channel.send({content: imageText, files: [image]});
+            }
+
+            if (message.deletable){
+                await message.delete();
+            }
+        }
+    }
+}
+
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(client, message) {
@@ -9,29 +37,9 @@ module.exports = {
             return;
         }
 
-        // check for safe word for not being transcribed
-        if (!message.content.includes('donot')){
-            // for every attachment (picture), grab the links of the images
-            let imageLinks = [];
-            for (entry of message.attachments.entries()){
-                for (element of entry){
-                    if (element instanceof Attachment) 
-                        imageLinks.push(element.attachment);
-                }
-            }
-
-            if (imageLinks.length > 0){
-                // convert each image to text
-                for (link of imageLinks){
-                    let imageText = await convertImageToText(link);
-                    const image = new AttachmentBuilder(link);
-                    await message.channel.send({content: imageText, files: [image]});
-                }
-
-                if (message.deletable){
-                    await message.delete();
-                }
-            }
-        }
+        transcribeImage(message)
+            .catch((error) => {
+            console.error(error);
+        });
 	},
 };
--- utils/tesseract/imageToText.js
+++ utils/tesseract/imageToText.js
@@ -4,6 +4,7 @@ module.exports = {
     async convertImageToText(link){
         const worker = await createWorker()
     
+        //load language and detect text
         await worker.loadLanguage('eng');
         await worker.initialize('eng');
         const { data: { text } } = await worker.recognize(link);
